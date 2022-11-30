80d5878e8f09f1407e482d691b472073a34b49ea
CoreSinText <muhammadrazak4@gmail.com>
2022-11-30 19:20:36 +0700

    update progress

--- src/routes/(main)/profile/+page.svelte
+++ src/routes/(main)/profile/+page.svelte
@@ -1,1 +1,13 @@
-s
+<script>
+	const profileImage = '/razak_profile.jpeg';
+</script>
+
+<section class="w-full h-screen flex justify-center items-center px-36">
+	<figure class="flex w-[800px] space-x-4 border-2 p-2 rounded-lg shadow-lg">
+		<img src={profileImage} alt="photo-profile" class="w-[200px] rounded-lg" />
+		<figcaption class="font-roboto text-lg">
+			Saya adalah seorang mahasiswa di Universitas Teknologi Yogyakarta, saya memiliki keinginanan
+			untuk meningkatkan skill yang saya miliki
+		</figcaption>
+	</figure>
+</section>
