8af9a94503957edcfabd9614fb596ab5b35bff53
CoreSinText <muhammadrazak4@gmail.com>
2022-11-30 19:08:01 +0700

    update progress

--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -1,10 +1,31 @@
 <script>
+	import { goto } from '$app/navigation';
+	import axios from 'axios';
+
 	let username = '';
 	let password = '';
+
+	$: submit = async () => {
+		const response = await axios({
+			method: 'post',
+			url: 'https://dummyjson.com/auth/login',
+			headers: {},
+			data: {
+				username,
+				password
+			}
+		});
+
+		if (response.status === 200) {
+			goto('/');
+		}
+		console.log(response);
+	};
 </script>
 
 <section class="w-screen h-screen flex justify-center items-center bg-blue-600 font-roboto">
 	<form
+		on:submit|preventDefault={submit}
 		action=""
 		class="flex flex-col w-[500px] h-[300px] shadow-lg justify-evenly p-4 rounded-md bg-blue-800 text-white"
 	>
